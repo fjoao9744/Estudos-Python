1e3ac854c87a29fb0de641d6eb5c7ce63b719926
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-08-11 21:26:47 -0300

    donatello

--- Testes/turtle/001.py
+++ Testes/turtle/001.py
@@ -1,2 +1,24 @@
-# -*- coding: utf-8 -*-
+from turtle import * 
+import os
+donatello = Turtle() 
+donatello.shape('turtle')
 
+while True:
+    n = input('digite pra onde deseja andar:')
+    os.system('cls')
+    
+    if n == 'd':
+        donatello.right(90)
+
+    if n == 's':
+        donatello.right(180)
+    if n == 'a':
+        donatello.right(-90)
+
+    if n == 'w':
+        donatello.right(0)
+    
+
+    
+      
+done() 
---
+++ Testes/turtle/002.py
@@ -0,0 +1,19 @@
+from turtle import * 
+
+tela = Screen()
+donatello = Turtle()
+donatello.shape('turtle')
+
+donatello.circle(100)
+
+donatello.fillcolor('purple')
+donatello.begin_fill()
+donatello.end_fill()
+
+
+
+
+
+
+
+done()
