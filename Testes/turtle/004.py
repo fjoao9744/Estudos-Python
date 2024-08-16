9dd40fb0a5d6025739765b955bee152c49d0330f
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-08-16 16:12:10 -0300

    Create 004.py

---
+++ Testes/turtle/004.py
@@ -0,0 +1,28 @@
+from turtle import *
+
+leonardo = Turtle()
+leonardo.shape("turtle")
+
+leonardo.speed(1)
+leonardo.circle(50)
+
+leonardo.circle(-50)
+leonardo.right(-25)
+leonardo.forward(45)
+
+leonardo.right(25)
+leonardo.forward(200)
+
+leonardo.circle(-25)
+
+leonardo.right(90)
+
+leonardo.forward(50)
+leonardo.right(90)
+
+
+leonardo.forward(200)
+
+
+
+done()
