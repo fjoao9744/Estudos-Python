32a85eb41daf3e0ddbc12729fdd63a2603262572
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-09-20 23:51:14 -0300

    Update 04.py

---
+++ Testes/tk/04.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from turtle import *
+
+tela = Tk()
+
+def circulo():
+    donatello = Turtle()
+    donatello.shape("turtle")
+    donatello.circle(50)
+
+    mainloop()
+
+def quadrado():
+    donatello = Turtle()
+    for c in range(4):
+        donatello.forward(100)
+        donatello.right(-90)
+        
+    mainloop()
+
+def triangulo():
+    donatello = Turtle()
+    for c in range(3):
+        donatello.forward(100)
+        donatello.right(-120)
+
+
+    mainloop()
+
+
+Button(tela, text="circle", command=circulo).grid(row=0, column=0)
+Button(tela, text="square", command=quadrado).grid(row=0, column=1)
+Button(tela, text="triangle", command=triangulo).grid(row=0, column=2)
+
+
+
+tela.mainloop()
