54ab4ac4c477255c6d89548e47078b2c1581744a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-12-07 11:06:01 -0300

    desafio 85 86

--- CursoEmVideo/desafios/desafio-85-1.py
+++ CursoEmVideo/desafios/desafio-85-1.py
@@ -1,6 +1,5 @@
 lista = [[], []]
 
-
 for c in range(1, 8):
     n =  int(input(f'digite o {c}° numero: '))
     if n % 2 == 0:
@@ -12,4 +11,4 @@ lista[0].sort()
 lista[1].sort()
 
 print(f'os valores pares digitados foram: {lista[0]}')
-print(f'os vlaores impares digitados foram: {lista[1]} ')
+print(f'os valores impares digitados foram: {lista[1]} ')
--- CursoEmVideo/desafios/desafio-86.py
+++ CursoEmVideo/desafios/desafio-86.py
@@ -8,7 +8,5 @@ for c in range(0, 3):
     for i in range(0, 3):
         lista[i][c] = int(input(f'digite um valor para a posição {c, i}'))
         
-
-
 print(f'{lista[0][0:3]} \n{lista[1][0:3]} \n{lista[2][0:3]}')
 
