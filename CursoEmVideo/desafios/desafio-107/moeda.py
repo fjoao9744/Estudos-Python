83241618df57db9e92aa6d3c27702b1fe0374f15
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-03-16 16:29:34 -0300

    107

---
+++ CursoEmVideo/desafios/desafio-107/moeda.py
@@ -0,0 +1,11 @@
+def aumentar(num, p):
+    return num + ((num * p) // 100)
+    
+def diminuir(num, p):
+    return num - ((num * p) // 100)
+    
+def metade(num):
+    return num // 2
+    
+def dobro(num):
+    return num * 2
